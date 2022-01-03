b30d24bb24377d08f8a4c39ccb0e43e8bf352532
Erik Westrup <erik.westrup@gmail.com>
2022-01-03 20:22:33 +0100

    Added: test/nestedtext/scanner_test.rb

---
+++ test/nestedtext/scanner_test.rb
@@ -0,0 +1,23 @@
+require "stringio"
+
+require "test_helper"
+
+class ScannerTest < Minitest::Test
+  def test_empty
+    scanner = NestedText::LineScanner.new(StringIO.new("# comment\n#comment"))
+    assert scanner.empty?
+  end
+
+  def test_reading_when_empty
+    ntstring = "- single item"
+    scanner = NestedText::LineScanner.new(StringIO.new(ntstring))
+    refute scanner.empty?
+    line = scanner.read_next
+    assert_equal ntstring, line.line_content
+
+    assert scanner.empty?
+    assert_raises(NestedText::Errors::LineScannerIsEmpty) do
+      scanner.read_next
+    end
+  end
+end
