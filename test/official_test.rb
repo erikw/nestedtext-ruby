afc09ae20802ea6e189b24ea29f32706e4e9a3a2
Erik Westrup <erik.westrup@gmail.com>
2022-01-05 17:28:14 +0100

    Read directory names

--- test/official_test.rb
+++ test/official_test.rb
@@ -7,9 +7,9 @@ class OfficialTest < Minitest::Test
   # TODO: filter for load_success, load_error, dump_success & dump_error cases and indicate this in the method name # e.g.:
   # def test_load_success_dict_01
   # def test_dump_success_dict_01
-  cases.each do |phone_number|
-    define_method("test_#{phone_number}_has_7_characters") do
-      assert_equal(7, phone_number.length)
+  cases.each do |caze|
+    define_method("test_#{caze.name}") do
+      assert_equal(7, 5)
     end
   end
 end
