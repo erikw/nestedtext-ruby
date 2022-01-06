62f2fd7481982c52e1e384be87d18c647ed63ed8
Erik Westrup <erik.westrup@gmail.com>
2022-01-06 17:15:42 +0100

    Official succes cases run

--- test/official_test.rb
+++ test/official_test.rb
@@ -4,12 +4,13 @@ require_relative "official_tests/api/nestedtext_official_tests"
 
 class OfficialTest < Minitest::Test
   cases = NestedTextOfficialTests.load_test_cases
-  # TODO: filter for load_success, load_error, dump_success & dump_error cases and indicate this in the method name # e.g.:
-  # def test_load_success_dict_01
-  # def test_dump_success_dict_01
-  cases.each do |caze|
-    define_method("test_#{caze.name}") do
-      assert_equal(7, 5)
+  # TODO: define tests for: load_error, dump_success, dump_error
+
+  NestedTextOfficialTests.select_load_success(cases).each do |caze|
+    define_method("test_load_success_#{caze.name}") do
+      actual = NestedText.load_file(caze[:load][:in][:path])
+      expected = caze[:load][:out][:data]
+      assert_equal(expected, actual)
     end
   end
 end
