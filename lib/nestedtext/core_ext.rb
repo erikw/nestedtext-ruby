45ac030760a33b13a124a9b046879cf03c39eb29
Erik Westrup <erik.westrup@gmail.com>
2022-01-13 18:30:52 +0100

    Modified: lib/nestedtext/core_ext.rb lib/nestedtext/encode.rb

--- lib/nestedtext/core_ext.rb
+++ lib/nestedtext/core_ext.rb
@@ -9,12 +9,12 @@ end
 class Array
   # TODO: how pass level & indentatin with default values? and make it easy for developers to make their own to_nt?
   # Better to do like psych's #encode_with?
-  def to_nt(indentation: 4, **_kwargs)
-    indent = " " * indentation
+  def to_nt(level: 0, indentation: 4, **kwargs)
+    indent = " " * indentation * level
     rep = each.map do |e|
-      indent + NestedText.add_prefix("-", e.to_nt(indentation: indentation + 4))
+      indent + NestedText.add_prefix("-", e.to_nt(level: level + 1, indentation: indentation, **kwargs))
     end.join("\n")
-    rep.prepend("\n") if length > 0 && indentation > 0
+    rep.prepend("\n") if length > 0 && level > 0
     rep
   end
 end
