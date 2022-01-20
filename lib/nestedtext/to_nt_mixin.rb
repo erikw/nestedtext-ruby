832b60702286a442d9b5481f6fa22bedac4054a8
Erik Westrup <erik.westrup@gmail.com>
2022-01-20 11:13:21 +0100

    Deleted: lib/nestedtext/to_nt_mixin.rb

--- lib/nestedtext/to_nt_mixin.rb
+++
@@ -1,9 +0,0 @@
-require "nestedtext/dumper"
-require "nestedtext/encode_helpers"
-
-# TODO: better module names for end users. It would be nice if the could do just
-# require 'nestedtext/encoding'
-# to get mixin and th needed helper methods for encoding.
-
-module NestedText
-end
