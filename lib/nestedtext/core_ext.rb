40f0648a59ea3648b278054030337585270b2591
Erik Westrup <erik.westrup@gmail.com>
2022-01-27 14:37:50 +0100

    Fix core ext rdoc names by using seimicolon

--- lib/nestedtext/core_ext.rb
+++ lib/nestedtext/core_ext.rb
@@ -1,13 +1,15 @@
 require "nestedtext/encode_helpers"
 
+# Extension of Ruby core types with the NestedText::NTEncodeMixin.
+#
 # TODO: add encoding of more Ruby native classes like Integer, Float etc plus commons like Set,....? Not covered in NestedText language.
 # Or leave this to a schema validator 3rd party plugin maybe? And replace my custom class decoding (and also encoding?)?
 # Or both: add encoding/decoding of more native classes, and allow decoding + applying a schema with 3rd party.
 # Or encourage using Marshal from core?
 
-class String include NestedText::NTEncodeMixin end
-class Array include NestedText::NTEncodeMixin end
-class Hash include NestedText::NTEncodeMixin end
+class String include NestedText::NTEncodeMixin; end
+class Array include NestedText::NTEncodeMixin; end
+class Hash include NestedText::NTEncodeMixin; end
 
 class NilClass
   include NestedText::NTEncodeMixin
