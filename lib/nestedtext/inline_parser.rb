346455d82ba804a0481e6d7eae05dc5f99d69fbf
Erik Westrup <erik.westrup@gmail.com>
2022-01-28 14:07:35 +0100

    Improve method naming in InlineParser

--- lib/nestedtext/inline_parser.rb
+++ lib/nestedtext/inline_parser.rb
@@ -4,7 +4,7 @@ require 'nestedtext/errors_internal'
 require 'nestedtext/scanners'
 
 module NestedText
-  # A LL(1) recursive descent parser for NT.
+  # A LL(1) recursive descent parser for inline NT types.
   class InlineParser
     def initialize(line)
       @inline_scanner = InlineScanner.new(line)
@@ -16,6 +16,26 @@ module NestedText
         raise Errors::ParseInlineExtraCharactersAfterDelimiterError.new(@inline_scanner.line, @inline_scanner.pos,
                                                                         @inline_scanner.remaining)
       end
+      result
+    end
+
+    def parse_any
+      return nil if @inline_scanner.peek.nil?
+
+      result = nil
+      # Trim leading whitespaces
+      @inline_scanner.read_next while !@inline_scanner.empty? && [' ', "\t"].include?(@inline_scanner.peek)
+      result = case @inline_scanner.peek
+               when '{'
+                 parse_dict
+               when '['
+                 parse_list
+               else # Inline string
+                 parse_string
+               end
+
+      # Trim trailing whitespaces
+      @inline_scanner.read_next while !@inline_scanner.empty? && [' ', "\t"].include?(@inline_scanner.peek)
       result
     end
 
@@ -40,71 +60,65 @@ module NestedText
       key.join.strip
     end
 
-    def parse_any
-      return nil if @inline_scanner.peek.nil?
+    def parse_dict
+      result = {}
+      first = true
+      loop do
+        @inline_scanner.read_next
+        break if first && @inline_scanner.peek == '}'
 
-      result = nil
-      # Trim leading whitespaces
-      @inline_scanner.read_next while !@inline_scanner.empty? && [' ', "\t"].include?(@inline_scanner.peek)
-      case @inline_scanner.peek
-      when '{'
-        result = {}
-        first = true
-        loop do
-          @inline_scanner.read_next
-          break if first && @inline_scanner.peek == '}'
-
-          first = false
-          key = parse_key
-          value = parse_any
-          result[key] = value
-          break unless @inline_scanner.peek == ','
-        end
-        if @inline_scanner.empty?
-          raise Errors::ParseInlineNoClosingDelimiterError.new(@inline_scanner.line,
-                                                               @inline_scanner.pos)
-        end
-        last_char = @inline_scanner.read_next
-        unless last_char == '}'
-          raise Errors::ParseInlineDictSyntaxError.new(@inline_scanner.line, @inline_scanner.pos - 1,
-                                                       last_char)
-        end
-
-      when '['
-        result = []
-        first = true # TODO: can be replaced by checking result.empty? below?
-        loop do
-          @inline_scanner.read_next
-          break if first && @inline_scanner.peek == ']'
-
-          first = false
-          result << parse_any
-          break unless @inline_scanner.peek == ','
-        end
-        if @inline_scanner.empty?
-          raise Errors::ParseInlineNoClosingDelimiterError.new(@inline_scanner.line,
-                                                               @inline_scanner.pos)
-        end
-        last_char = @inline_scanner.read_next
-
-        if last_char != ']'
-          if result[-1] == ''
-            raise Errors::ParseInlineMissingValueError.new(@inline_scanner.line,
-                                                           @inline_scanner.pos - 1)
-          end
-
-          raise Errors::ParseInlineListSyntaxError.new(@inline_scanner.line, @inline_scanner.pos - 1, last_char)
-        end
-      else # Inline string
-        inline_string = []
-        until @inline_scanner.empty? || ['{', '}', '[', ']', ','].include?(@inline_scanner.peek)
-          inline_string << @inline_scanner.read_next
-        end
-        result = inline_string.join.rstrip # Trim trailing whitespaces that lead up to next break point.
+        first = false
+        key = parse_key
+        value = parse_any
+        result[key] = value
+        break unless @inline_scanner.peek == ','
       end
-      # Trim trailing whitespaces
-      @inline_scanner.read_next while !@inline_scanner.empty? && [' ', "\t"].include?(@inline_scanner.peek)
+      if @inline_scanner.empty?
+        raise Errors::ParseInlineNoClosingDelimiterError.new(@inline_scanner.line,
+                                                             @inline_scanner.pos)
+      end
+      last_char = @inline_scanner.read_next
+      unless last_char == '}'
+        raise Errors::ParseInlineDictSyntaxError.new(@inline_scanner.line, @inline_scanner.pos - 1,
+                                                     last_char)
+      end
       result
+    end
+
+    def parse_list
+      result = []
+      first = true # TODO: can be replaced by checking result.empty? below?
+      loop do
+        @inline_scanner.read_next
+        break if first && @inline_scanner.peek == ']'
+
+        first = false
+        result << parse_any
+        break unless @inline_scanner.peek == ','
+      end
+      if @inline_scanner.empty?
+        raise Errors::ParseInlineNoClosingDelimiterError.new(@inline_scanner.line,
+                                                             @inline_scanner.pos)
+      end
+      last_char = @inline_scanner.read_next
+
+      if last_char != ']'
+        if result[-1] == ''
+          raise Errors::ParseInlineMissingValueError.new(@inline_scanner.line,
+                                                         @inline_scanner.pos - 1)
+        end
+
+        raise Errors::ParseInlineListSyntaxError.new(@inline_scanner.line, @inline_scanner.pos - 1, last_char)
+      end
+      result
+    end
+
+    def parse_string
+      inline_string = []
+      until @inline_scanner.empty? || ['{', '}', '[', ']', ','].include?(@inline_scanner.peek)
+        inline_string << @inline_scanner.read_next
+      end
+      inline_string.join.rstrip # Trim trailing whitespaces that lead up to next break point.
     end
   end
   private_constant :InlineParser
