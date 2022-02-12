81a783c7dbeaf61e44554b0b75691999bb5f1bab
Erik Westrup <erik.westrup@gmail.com>
2022-02-12 08:52:00 +0100

    Better module documentation

--- lib/nestedtext.rb
+++ lib/nestedtext.rb
@@ -7,9 +7,10 @@ require_relative 'nestedtext/encode_helpers'
 require_relative 'nestedtext/error'
 require_relative 'nestedtext/version'
 
-##
 # # NestedText
-# The main module in this library to use.
+# A ruby library for the human friendly data format NestedText (https://nestedtext.org/).
+#
+# Provided is support for decoding a NestedText file or string to Ruby data structures, as well as encoding Ruby objects to a NestedText file or string. Furthermore there is support for serialization and deserialization of custom classes.
 #
 # See {file:README.md} for documentation on Types, Strict Mode and Custom Classes.
 module NestedText
